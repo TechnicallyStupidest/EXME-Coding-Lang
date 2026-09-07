c45832c8d9cd1189e045cea58b6a43dc6e886985
TechnicallyStupidest <tigee.dev.freelance@gmail.com>
2026-09-06 23:27:48 -0400

    Delete EXME directory

--- EXME/vscode_extension/exme_install_vscode_windows.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-set "DEST=%USERPROFILE%\.vscode\extensions\exme-manual-language-2.0.0"
-if exist "%DEST%" rmdir /s /q "%DEST%"
-mkdir "%DEST%"
-xcopy /e /i /y "%~dp0*" "%DEST%\" >nul
-if errorlevel 1 exit /b %errorlevel%
-echo EXME Manual VS Code extension installed. Restart VS Code.
