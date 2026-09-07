5bcfd0a39b61f88d100f81164f65c80d7c20b2f3
TechnicallyStupidest <tigee.dev.freelance@gmail.com>
2026-09-06 22:43:21 -0400

    Delete EXME_MANUAL directory

--- EXME_MANUAL/compiler/exme_build_linux.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-set -eu
-cd "$(dirname "$0")"
-g++ exme_main.cpp exme_manual.cpp -O3 -std=c++17 -DNDEBUG -s -o exme
--- EXME_MANUAL/vscode_extension/exme_install_vscode_linux.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-set -eu
-DEST="$HOME/.vscode/extensions/exme-manual-language-2.0.0"
-rm -rf "$DEST"
-mkdir -p "$DEST"
-cp -R "$(dirname "$0")"/. "$DEST"/
-echo "EXME Manual VS Code extension installed. Restart VS Code."
